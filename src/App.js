54a3620461b878513bb622a28d3cee8d0e53a1bf
Your Name <behnooda81@gmail.com>
2023-02-14 12:49:39 +0330

    add spinner

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import './App.css';
 
 const App = () => {
 
+  const [loading, setLoading] = useState(false);
   const [getContacts, setContacts] = useState([]);
   
   return (
     <div className="App">
       <Navbar />
-      <Contacts contacts={getContacts}/>
+      <Contacts contacts={getContacts} loading={loading}/>
     </div>
   );
 }
--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,7 +1,8 @@
 import Contact from "./Contact";
+import Spinner from "../Spinner";
 import { CURRENTLINE, ORANGE, PINK } from "../../helpers/colors";
 
-const Contacts = ({ contacts }) => {
+const Contacts = ({ contacts, loading }) => {
     return (
         <>
             <section className="container">
@@ -18,29 +19,33 @@ const Contacts = ({ contacts }) => {
                     </div>
                 </div>
             </section>
-            <section className="container">
-                <div className="row">
-                    {contacts.length > 0
-                        ? contacts.map((c) => <Contact key={c.id} contact={c} />)
-                        : (
-                            <div
-                                className="text-center py-5"
-                                style={{ backgroundColor: CURRENTLINE }}
-                            >
-                                <p className="h3" style={{ color: ORANGE }}>
-                                    مخاطب یافت نشد...
-                                </p>
-                                <img
-                                    src={require("../../assets/no-found.gif")}
-                                    alt="پیدا نشد"
-                                    className="w-25"
-                                />
-                            </div>
-                        )
-                    }
-                    {/* <Contact /> */}
-                </div>
-            </section>
+            {
+                loading ? <Spinner /> : (
+                    <section className="container">
+                        <div className="row">
+                            {contacts.length > 0
+                                ? contacts.map((c) => <Contact key={c.id} contact={c} />)
+                                : (
+                                    <div
+                                        className="text-center py-5"
+                                        style={{ backgroundColor: CURRENTLINE }}
+                                    >
+                                        <p className="h3" style={{ color: ORANGE }}>
+                                            مخاطب یافت نشد...
+                                        </p>
+                                        <img
+                                            src={require("../../assets/no-found.gif")}
+                                            alt="پیدا نشد"
+                                            className="w-25"
+                                        />
+                                    </div>
+                                )
+                            }
+                            {/* <Contact /> */}
+                        </div>
+                    </section>
+                )
+            }
         </>
     );
 }
